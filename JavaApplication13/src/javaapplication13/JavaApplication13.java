b696bca6ff85a88efcfb8b481da56f4c84ff4fb5
Rizki354 <rizkinasrulloh88@gmail.com>
2024-02-06 12:47:58 +0700

    Add project

---
+++ JavaApplication13/src/javaapplication13/JavaApplication13.java
@@ -0,0 +1,10 @@
+package javaapplication13;
+
+import javax.swing.JOptionPane;
+
+public class JavaApplication13 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello, World!", "Info", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+}
