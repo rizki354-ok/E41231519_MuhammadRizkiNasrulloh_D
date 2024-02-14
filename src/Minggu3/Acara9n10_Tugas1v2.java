0d4745534bd4190e0dc057e5255f83e32f6666ed
Rizki354 <rizkinasrulloh88@gmail.com>
2024-02-14 15:56:12 +0700

    Update Acara9n10_Tugas1v2.java

--- src/Minggu3/Acara9n10_Tugas1v2.java
+++ src/Minggu3/Acara9n10_Tugas1v2.java
@@ -1,20 +1,17 @@
 package Minggu3;
-
 import java.util.Scanner;
-
 public class Acara9n10_Tugas1v2 {
-
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scan=new Scanner(System.in);
         System.out.print("Masukkan jumlah angka yang akan dimasukkan= ");
-        int jumlahAngka = scan.nextInt();
-        int[] angka = new int[jumlahAngka];
-        for (int i = 0; i < angka.length; i++) {
+        int jumlahAngka=scan.nextInt();
+        int[] angka=new int[jumlahAngka];
+        for(int i=0;i<angka.length;i++){
             System.out.print("");
-            angka[i] = scan.nextInt();
+            angka[i]=scan.nextInt();
         }
-        for (int number : angka) {
-            System.out.println(number);
+        for(int i:angka){
+            System.out.print(i+" ");
         }
 //        for(int i=0;i<=angka.length-1;i++){
 //            System.out.print(angka[i]+" ");
